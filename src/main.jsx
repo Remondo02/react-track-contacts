4176399ce55d5198365a3303d7ca896597210358
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2023-11-22 15:32:39 +0900

    Nested routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import "./index.css"
 import Root from "./routes/root"
-import ErrorPage from "./error-page";
+import ErrorPage from "./error-page"
+import Contact from "./routes/contact"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ])
 
